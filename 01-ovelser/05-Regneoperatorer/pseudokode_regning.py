7260d3f3e9be8948492b794096eca8b9557280d1
Your Name <you@example.com>
2025-09-09 09:36:25 +0200

    Add pseudocode for arithmetic operations in new script

---
+++ 01-ovelser/05-Regneoperatorer/pseudokode_regning.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-09 09:18:42
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-09 09:27:40
+# Lag to variabler for tall
+tall1 = 3
+tall2 = 2
+tall3 = 4
+# Addisjon
+# sum1 = tall1 + tall2
+# print(sum1)
+# Subtraksjon
+# sum2 = tall1 - tall2
+# print(sum2)
+# # Multiplikasjon
+# resultat1 = tall1 * tall2
+# print(resultat1)
+# Divisjon
+# resultat2 = tall1 / tall2
+# print(resultat2)
+# Potensregning
+# resultat3 = tall1 ** tall2
+# print(resultat3)
+# Test av regnestykket
+
+resultat4 = tall1 + tall2 * tall3
+print(resultat4)
