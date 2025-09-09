ddc120abefffe2aa0afe59792bf9e710a8eabc4d
William Berge Grønsberg <wil27051@skole.telemarkfylke.no>
2025-09-09 21:23:55 +0200

    Update pseudocode and Python script with improved comments and error handling for guessing game

--- 02-oppgaver/05-tippespill/opg.py
+++ 02-oppgaver/05-tippespill/opg.py
@@ -2,24 +2,30 @@
 # @Author: William Berge Groensberg
 # @Date:   2025-09-09 10:58:29
 # @Last Modified by:   William Berge Groensberg
-# @Last Modified time: 2025-09-09 11:22:50
+# @Last Modified time: 2025-09-09 21:23:36
 # importer random
 import random
 
 # lag variabelen talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet 
-# gi variabelen talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet et randomt tall
+# gi variabelen talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet et randomt tall mellom 1 og 10
 talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet = random.randint(1, 10)
 
 #     lag variabelen inputtSomBrukerGirTerminalen
 #     gi variabelen inputtSomBrukerGirTerminalen verdi int inputt
-inputtSomBrukerGirTerminalen = int(input("Hvilke tall gjetter du: "))
+try: 
+    inputtSomBrukerGirTerminalen = int(input("Hvilke tall gjetter du: "))
+#     sjekk om bruker skrev in et tall
+#     gir all verdierror print som sier at bruker må skrive in et tall
+
+except ValueError:
+    print("Du må skrive inn et heltall.\nKjør på nytt for å prøve igjen...")
+    exit()
 
 # så lenge inputtSomBrukerGirTerminalen ikke er lik talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet
 while inputtSomBrukerGirTerminalen != talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet:
     
     
     
-# spør bruker hvilke tall den jetter
     
     #     hvis inputtSomBrukerGirTerminalen er større en talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet
     if inputtSomBrukerGirTerminalen > talletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet:
@@ -30,6 +36,7 @@ while inputtSomBrukerGirTerminalen != talletSomSkalBliGjettetAvBrukerenSomBruker
     # print tallet er høyere
         print("Tallet er høyere")
     
+    # spør bruker hvilke tall den jetter
     inputtSomBrukerGirTerminalen = int(input("Hvilke tall gjetter du: "))
 # print du gjettet rinktig tall
 print("du gjettet riktig")
