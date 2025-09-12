800323eee4a44e937ecf9ffbfdaf99fa43318cd7
William Berge Grønsberg <wil27051@skole.telemarkfylke.no>
2025-09-12 12:52:28 +0200

    Add initial implementation of guessing game with user input and feedback

---
+++ 02-oppgaver/05-tippespill/opg-itmb014.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-09 10:58:29
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-12 12:35:44
+# importer random
+import random
+
+# lag variabelen heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet 
+# gi variabelen heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet et randomt tall
+heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet = random.randint(1, 10)
+
+#     lag variabelen inputtSomBrukerGirTerminalen
+#     gi variabelen inputtSomBrukerGirTerminalen verdi int inputt
+inputtSomBrukerGirTerminalen = int(input("Hvilke tall gjetter du: "))
+
+# så lenge inputtSomBrukerGirTerminalen ikke er lik heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet
+while inputtSomBrukerGirTerminalen != heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet:
+    
+    
+    
+# spør bruker hvilke tall den jetter
+    
+    #     hvis inputtSomBrukerGirTerminalen er større en heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet
+    if inputtSomBrukerGirTerminalen > heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet:
+    #         print tallet er lavere
+        print("tallet er lavere")
+    #     eller inputtSomBrukerGirTerminalen er mindre en heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet
+    elif inputtSomBrukerGirTerminalen < heltalletSomSkalBliGjettetAvBrukerenSomBrukerProgrammet:
+    # print tallet er høyere
+        print("Tallet er høyere")
+    
+    inputtSomBrukerGirTerminalen = int(input("Hvilke tall gjetter du: "))
+# print du gjettet rinktig tall
+print("du gjettet riktig")
