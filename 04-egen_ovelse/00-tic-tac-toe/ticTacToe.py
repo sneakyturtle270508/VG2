a9421730255dec617a1d9f150e2403334ae64d96
William Berge Grønsberg <wil27051@skole.telemarkfylke.no>
2025-09-12 12:24:57 +0200

    Refactor Tic Tac Toe code for improved readability and organization

--- 04-egen_ovelse/00-tic-tac-toe/ticTacToe.py
+++ 04-egen_ovelse/00-tic-tac-toe/ticTacToe.py
@@ -1,27 +1,17 @@
-# # -*- coding: utf-8 -*-
-# # @Author: William Berge Groensberg
-# # @Date:   2025-09-11 14:16:31
-# # @Last Modified by:   William Berge Groensberg
-# # @Last Modified time: 2025-09-11 15:01:44
-
-
-player1Score = 0 
-player2Score = 0
-drawScore = 0
-
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-11 14:16:31
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-11 15:54:27
+# initialisering
+board = [1,2,3,4,5,6,7,8,9]
 spiller1 = []
 spiller2 = []
-board = [1,2,3,4,5,6,7,8,9]
 
 winning_combo = [
-    {1,2,3},
-    {4,5,6},
-    {7,8,9},
-    {1,4,7},
-    {2,5,8},
-    {3,6,9},
-    {1,5,9},
-    {3,5,7},
+    {1,2,3}, {4,5,6}, {7,8,9},
+    {1,4,7}, {2,5,8}, {3,6,9},
+    {1,5,9}, {3,5,7},
 ]
 
 def print_board():
@@ -30,20 +20,27 @@ def print_board():
     print(f"{board[3]} | {board[4]} | {board[5]}")
     print("--+---+--")
     print(f"{board[6]} | {board[7]} | {board[8]}")
+    print("\nSpiller 1: ", spiller1)
+    print("Spiller 2: ", spiller2)
+    print("\n")  # ekstra linje for å gjøre det ryddig
 
 while True:
-    # Spiller 1
     print_board()
+    
+    # Spiller 1
     valgt_tall = int(input("Spiller 1, velg tall: "))
     if valgt_tall not in board:
         print("Ugyldig trekk, prøv igjen.")
         continue
     spiller1.append(valgt_tall)
     board[board.index(valgt_tall)] = 'X'
+    
     if any(combo.issubset(spiller1) for combo in winning_combo):
         print_board()
         print("Spiller 1 vant!")
         break
+
+    # Sjekk for uavgjort
     if all(isinstance(x, str) for x in board):
         print_board()
         print("Uavgjort!")
@@ -57,10 +54,13 @@ while True:
         continue
     spiller2.append(valgt_tall)
     board[board.index(valgt_tall)] = 'O'
+    
     if any(combo.issubset(spiller2) for combo in winning_combo):
         print_board()
         print("Spiller 2 vant!")
         break
+
+    # Sjekk for uavgjort igjen
     if all(isinstance(x, str) for x in board):
         print_board()
         print("Uavgjort!")
