402ce58ad5adefe0da5bcb157e08bc0394f077af
Your Name <you@example.com>
2025-09-16 11:03:22 +0200

    Implement currency exchange logic and update pseudocode for clarity

--- 01-ovelser/07-Heltallsdiv-modulo/heltallsdiv-modulo.py
+++ 01-ovelser/07-Heltallsdiv-modulo/heltallsdiv-modulo.py
@@ -1,26 +1,41 @@
-# -*- coding: utf-8 -*-
-# @Author: William Berge Groensberg
-# @Date:   2025-09-16 08:22:42
-# @Last Modified by:   William Berge Groensberg
-# @Last Modified time: 2025-09-16 08:28:51
+# # -*- coding: utf-8 -*-
+# # @Author: William Berge Groensberg
+# # @Date:   2025-09-16 08:22:42
+# # @Last Modified by:   William Berge Groensberg
+# # @Last Modified time: 2025-09-16 10:22:08
 
 
 
-teller = 300
-nevner = 100
+# teller = 300
+# nevner = 100
 
-# divisjon
-# resultat = teller / nevner
+# # divisjon
+# # resultat = teller / nevner
+
+# # print(resultat)
+
+# # helltalsdivisjon
+# resultat = teller // nevner
 
 # print(resultat)
 
-# helltalsdivisjon
-resultat = teller // nevner
+# # modulo
 
-print(resultat)
+# resultat = teller % nevner
 
-# modulo
+# print(resultat)
 
-resultat = teller % nevner
 
-print(resultat)
+
+
+
+kjopePris = int(input("Hva er kjøpesummen: "))
+# lag variabel betaltPris og giv input og mota svar om hvordan kunden vil betale
+betaltPris = int(input("hva betaler du med "))
+
+
+
+# lag variabel sjekkerIgjen og giv den verdi kjopePris % betaltPris
+sjekkerIgjen = betaltPris % kjopePris
+
+print(sjekkerIgjen)
