d24a16541e066db27aabbdd79c3733e1fe564f6e
Your Name <you@example.com>
2025-09-16 08:39:09 +0200

    Add initial implementation of integer division and modulo operations

---
+++ 01-ovelser/07-Heltallsdiv-modulo/heltallsdiv-modulo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-16 08:22:42
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-16 08:28:51
+
+
+
+teller = 300
+nevner = 100
+
+# divisjon
+# resultat = teller / nevner
+
+# print(resultat)
+
+# helltalsdivisjon
+resultat = teller // nevner
+
+print(resultat)
+
+# modulo
+
+resultat = teller % nevner
+
+print(resultat)
---
+++ 04-egen_ovelse/00-pedo-filer/modulo.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-16 08:19:20
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-16 08:22:28
