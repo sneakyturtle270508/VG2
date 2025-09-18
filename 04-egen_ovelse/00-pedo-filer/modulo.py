9d0ec4541b292915e83e9f54ed4c53d1095db62d
Your Name <you@example.com>
2025-09-18 08:13:23 +0200

    Remove unused modulo.py file

--- 04-egen_ovelse/00-pedo-filer/modulo.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: William Berge Groensberg
-# @Date:   2025-09-16 08:19:20
-# @Last Modified by:   William Berge Groensberg
-# @Last Modified time: 2025-09-16 08:22:28
