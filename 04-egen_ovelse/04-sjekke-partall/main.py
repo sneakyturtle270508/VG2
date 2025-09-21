5b8aec6cec9d10e5ce682afe15aec102d4aaadec
William Berge Grønsberg <wil27051@skole.telemarkfylke.no>
2025-09-21 12:43:51 +0200

    adding some modifications on the client and readme. and making a numberchecker

---
+++ 04-egen_ovelse/04-sjekke-partall/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: William Berge Groensberg
+# @Date:   2025-09-21 12:39:49
+# @Last Modified by:   William Berge Groensberg
+# @Last Modified time: 2025-09-21 12:41:49
+
+
+
+helltallFraBruker = int(input("skriv in et helltall: "))
+
+if helltallFraBruker % 2 == 1:
+    print("dette er et oddetall")
+else:
+    print("dette er partall")
